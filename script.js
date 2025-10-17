7fdcb45a7604c34f827a2e0be8a8701b07598a9e
Sloth |-/ <softerslothr@gmail.com>
2025-10-17 07:03:16 -0500

    Refactor copyIP function in script.js

--- script.js
+++ script.js
@@ -4,21 +4,3 @@ function copyIP() {
       alert("Server IP copied: play.xyrealn.com");
 
     }
-//server status
-async function getServerStatus() {
-  const statusElement = document.getElementById('server-status');
-
-  try {
-    const response = await fetch('https://api.mcsrvstat.us/2/play.xyrealn.com');
-    const data = await response.json();
-
-    if (data.online) {
-      statusElement.textContent = `Online with ${data.players.online}/${data.players.max} players!`;
-    } else {
-      statusElement.textContent = 'Server is offline.';
-    }
-  } catch (error) {
-    statusElement.textContent = 'Unable to check status.';
-  }
-}
-
